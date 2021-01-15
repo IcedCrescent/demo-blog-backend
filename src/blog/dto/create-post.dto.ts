9bcc38d4305d1d575e43e51c60c0b20f6aa776f1
IcedCrescent <35650172+IcedCrescent@users.noreply.github.com>
2021-01-15 14:17:16 +0700

    Initial commit

---
+++ src/blog/dto/create-post.dto.ts
@@ -0,0 +1,7 @@
+export class CreatePostDTO {
+    readonly title: string;
+    readonly description: string;
+    readonly author: string;
+    readonly body: string;
+    readonly date_posted: string;
+}
